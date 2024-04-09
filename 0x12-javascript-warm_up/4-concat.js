8eec90c340df700ce5f1703b7cf588cd4807b9ce
Solomon 'Darth Jade-i' Okomowho <solomon.okomowho@gmail.com>
2024-04-09 17:03:15 +0100

    file: Created 4-concat.js as an executable file - Task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,16 +1,2 @@
 #!/usr/bin/node
-
-// Get the first command-line argument (index 2) and store it in a variable
-const arg1 = process.argv[2];
-
-// Get the second command-line argument (index 3) and store it in a variable
-const arg2 = process.argv[3];
-
-// Check if both arguments are provided
-if (arg1 !== undefined && arg2 !== undefined) {
-  // If arguments are defined, concatenate them into a string
-  console.log(arg1 + ' is ' + arg2);
-} else {
-  // If any argument is missing, log an appropriate message
-  console.log('Insufficient arguments provided');
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
