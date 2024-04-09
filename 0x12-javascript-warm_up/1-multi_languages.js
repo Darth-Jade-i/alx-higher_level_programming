5251c70e1dbc2401125f944a223a1ff01497ea39
Solomon 'Darth Jade-i' Okomowho <solomon.okomowho@gmail.com>
2024-04-09 16:20:11 +0100

    file: Created 1-multi_languages.js as an executable file - Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+//  This script print out a message to output.
+//  Tried writing different variables for each message
+//  That didn't work
+
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
