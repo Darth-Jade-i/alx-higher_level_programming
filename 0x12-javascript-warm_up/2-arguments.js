7af3b723cb973d730d062584a9517f0ee61ea516
Solomon 'Darth Jade-i' Okomowho <solomon.okomowho@gmail.com>
2024-04-09 16:35:43 +0100

    file: Created 2-arguments.js as an executable file - Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+//  Get the number of command-line arguments
+const count = process.argv.length;
+
+//  Check the number of arguments and provide appropriate output
+if (count === 2) {
+    console.log('No argument');
+} else if (count === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
