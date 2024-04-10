e5b9c055c7eb9f842b8a2be377d40b4a2735e7ae
Solomon 'Darth Jade-i' Okomowho <solomon.okomowho@gmail.com>
2024-04-10 18:55:17 +0100

    Made corrections to make files semistandard compliant

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if ((w < 1) || (h < 1)) {
-
-    } else if ((isNaN(w)) || (isNaN(h))) {
-
-    } else {
-	    this.width = w;
-	    this.height = h;
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-	    let row = '';
-	    for (let j = 0; j < this.width; j++) {
-        row += 'X';
-	    }
-	    console.log(row);
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
   }
-};
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,18 +2,18 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
-	    this.width = w;
-	    this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-	    let row = '';
-	    for (let j = 0; j < this.width; j++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
         row += 'X';
-	    }
-	    console.log(row);
+      }
+      console.log(row);
     }
   }
 
