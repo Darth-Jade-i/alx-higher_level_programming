f9a25cbe4d5e99c7ba2d03d7a7d2d8a4d0c3e117
Solomon 'Darth Jade-i' Okomowho <solomon.okomowho@gmail.com>
2024-04-15 18:34:26 +0100

    edited model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,32 +3,12 @@
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
 
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    try:
-        # Create database engine
-        engine = create_engine(
-            f"mysql+mysqldb://{username}:{password}@localhost/{database_name}",
-            pool_pre_ping=True)
-
-        # Create tables based on defined models
-        Base.metadata.create_all(engine)
-
-        print("Tables created successfully.")
-
-    except SQLAlchemyError as e:
-        print(f"Error creating tables: {e}")
-        sys.exit(1)
-
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    main()
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2],
+                                                    sys.argv[3]),
+        pool_pre_ping=True)
+            Base.metadata.create_all(engine)
