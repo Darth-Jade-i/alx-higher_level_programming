73c6a8d932cbf02c289b84a8dd94e0be073e42ee
Solomon 'Darth Jade-i' Okomowho <solomon.okomowho@gmail.com>
2024-04-16 15:56:11 +0100

    Another attempt on task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,5 +15,7 @@ if __name__ == "__main__":
 
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+        print(
+            "{}: {}".format(
+                state.id, state.name))
+        session.close()
